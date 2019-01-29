7710624120f1ca95789c77f6ed7c069afd24aa33
Simon Cass <scass1991@gmail.com>
2019-01-29 16:08:42 +0000

    testing added for password equality:

--- spec/features/user_can_sign_up_spec.rb
+++ spec/features/user_can_sign_up_spec.rb
@@ -13,4 +13,17 @@ RSpec.feature 'Home Page', type: :feature do
     expect(current_path).to eq '/posts'
     expect(page).to have_content('Account created successfully!')
   end
+
+  scenario "A user can only sign up if passwords are equal" do
+    visit "/"
+    click_link "Sign Up"
+    fill_in "user_name", with: "Officer Jenny"
+    fill_in "user_email", with: "Jenny@PalletTownPD.com"
+    fill_in "user_password", with: "ihatecrime"
+    fill_in "user_password_confirmation", with: "ih8crime"
+    click_button "Sign Up"
+
+    expect(current_path).to eq '/users'
+    expect(page).to have_content("Couldn't create account")
+  end
 end
