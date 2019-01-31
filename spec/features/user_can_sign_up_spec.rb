f4b68ec0f18cf2b387d0012af16a992f401e38ff
Connor Kam-Cheong <connorkc42@gmail.com>
2019-01-31 11:23:29 +0000

    Add user input validation for sign up
    Co-Authored-By: Simon Cass <scass91@users.noreply.github.com>

--- spec/features/user_can_sign_up_spec.rb
+++ spec/features/user_can_sign_up_spec.rb
@@ -16,7 +16,20 @@ RSpec.feature 'Home Page', type: :feature do
     expect(page).to have_content('Account created successfully!')
   end
 
-  scenario 'A user can only sign up if passwords are equal' do
+  scenario 'A user must sign up with an email address' do
+    visit '/'
+    click_link 'Sign Up'
+    fill_in 'user_name', with: 'Officer Jenny'
+    fill_in 'user_password', with: 'ihatecrime'
+    fill_in 'user_password_confirmation', with: 'ihatecrime'
+    click_button 'Sign Up'
+
+    expect(current_path).to eq '/users'
+    expect(page).to have_content("couldn't create account")
+    expect(page).to have_content("Email can't be blank")
+  end
+
+  scenario "A user's passwords must be equal" do
     visit '/'
     click_link 'Sign Up'
     fill_in 'user_name', with: 'Officer Jenny'
@@ -26,11 +39,11 @@ RSpec.feature 'Home Page', type: :feature do
     click_button 'Sign Up'
 
     expect(current_path).to eq '/users'
-    expect(page).to have_content("Couldn't create account")
+    expect(page).to have_content("couldn't create account")
   end
 
-  scenario 'A user cannot sign up with more than 10 character password' do
-    message = "Password length must be between 6-10 characters."
+  scenario "A user's password must be 10 characters or shorter" do
+    message = 'Password is too long (maximum is 10 characters)'
     visit '/'
     click_link 'Sign Up'
     fill_in 'user_name', with: 'Edam Brie'
@@ -40,6 +53,22 @@ RSpec.feature 'Home Page', type: :feature do
     click_button 'Sign Up'
 
     expect(current_path).to eq '/users'
+    expect(page).to have_content("couldn't create account")
+    expect(page).to have_content(message)
+  end
+
+  scenario "A user's password must be 6 characters or longer" do
+    message = 'Password is too short (minimum is 6 characters)'
+    visit '/'
+    click_link 'Sign Up'
+    fill_in 'user_name', with: 'Edam Brie'
+    fill_in 'user_email', with: 'cheese@email.com'
+    fill_in 'user_password', with: 'brie'
+    fill_in 'user_password_confirmation', with: 'brie'
+    click_button 'Sign Up'
+
+    expect(current_path).to eq '/users'
+    expect(page).to have_content("couldn't create account")
     expect(page).to have_content(message)
   end
 end
