d4a27405cf22fcc5695d5cb540a1f5d2968a3541
Connor Kam-Cheong <connorkc42@gmail.com>
2019-02-01 12:13:22 +0000

    Add tests for Sessions get and post
    Co-Authored-By: null <anhuiyang@users.noreply.github.com>

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -1,5 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe SessionsController, type: :controller do
-  
+ let!(:user) do
+   User.create(
+     name: 'Officer Jenny',
+     email: 'Jenny@PalletTownPD',
+     password: 'ihatecrime'
+   )
+ end
+
+  describe 'GET /new' do
+    get :new
+    expect(response). to have_http_status(200)
+  end
+
+  describe 'POST /' do
+    it 'responds with 200' do
+      post :create, params: { user: {
+        email: 'Jenny@PalletTownPD',
+        password: 'ihatecrime'
+      } }
+
+      expect(response).to redirect_to(posts_url)
+    end
+  end
 end
