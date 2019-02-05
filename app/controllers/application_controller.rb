0591dd7ac79f45736b885bcf435ab2fe56c78b38
Connor Kam-Cheong <connorkc42@gmail.com>
2019-02-05 16:42:19 +0000

    Clean up code

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,9 +8,4 @@ class ApplicationController < ActionController::Base
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
-
-  def authorize
-    redirect_to login_path, alert: 'You must login to access this page.' if
-    current_user.nil?
-  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,6 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  get 'pages/secret'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
   get 'users/new' => 'users#new', as: :new_user
   post 'users' => 'users#create'
 
