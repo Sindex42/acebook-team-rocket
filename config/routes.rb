5c701ac94a2f9c059244148b746f3b2d3af8322d
Connor Kam-Cheong <connorkc42@gmail.com>
2019-02-06 17:25:35 +0000

    Add json response for posts#index
    Co-Authored-By: Simon Cass <scass91@users.noreply.github.com>

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,12 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  namespace :api do
+    namespace :v1 do
+      resources :posts, only: [:index]
+    end
+  end
+
   get 'users/new' => 'users#new', as: :new_user
   post 'users' => 'users#create'
 
@@ -9,5 +15,6 @@ Rails.application.routes.draw do
   delete '/logout' => 'sessions#destroy'
 
   resources :posts
+
   root to: 'welcome#index'
 end
