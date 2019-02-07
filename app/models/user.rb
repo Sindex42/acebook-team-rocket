c71a22fcf2dbfb28bfa2b58124ba52c75c46b54e
Hui An Yang <hecatie36@hotmail.com>
2019-02-07 18:12:34 +0000

    added user association to post

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
+  has_many :posts
   has_secure_password
   validates :email, presence: true, uniqueness: true
   validates :password, length: { minimum: 6, maximum: 10 }, on: :create
--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190129123022) do
+ActiveRecord::Schema.define(version: 20190207173652) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "posts", force: :cascade do |t|
-    t.string "message"
+    t.text "message"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
